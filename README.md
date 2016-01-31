aec03718db44cc17b187122754dd3f15d7141c0e
George Lee <keoki.lee@gmail.com>
2016-01-30 21:34:38 -1000

    Create config files and initial project.

--- README.md
+++ README.md
@@ -15,21 +15,7 @@ Running the project
 Clone this project and run the following commands.
 
 ```
-> npm install express
 > npm start
-```
-
-Advanced
-===================
-
-The project comes with tasks that will watch the js files in app. When they change, the files will be copied to build/ automatically. If you have the Livereload browser extension, this task will also reload the page in your browser.
-
-To run the task:
-
-```
-> npm -g install gulp
-> npm install
-> gulp
 ```
 
 Notes
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: process.cwd(),
+  debug: true,
+  devtool: 'eval-source-map',
+  entry: {
+    angular2: [
+      'rxjs',
+      'reflect-metadata',
+      'angular2/core',
+      'angular2/router',
+      'angular2/http',
+    ],
+    app: './src/app'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts', 'tslint'],
+        exclude: /node_modules/
+      }
+    ],
+    noParse: [ /angular2\/bundles\//]
+  },
+  output: {
+    path: path.join(process.cwd(), 'build'),
+    publicPath: '/',
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map',
+    chunkFilename: '[id].chunk.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Fluidity Generator' }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'development'
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.ts', '.js', '.json', '.css', '.html']
+  }
+}
